8cef65109050638bba9a7f136f8617d4460a368a
Yaolin <44204509+YaolinGe@users.noreply.github.com>
2022-02-25 13:19:00 +0100

    Updated_data_interpolator

--- Test/Field/Grid/test_grid.py
+++ Test/Field/Grid/test_grid.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from GOOGLE.Field.Grid.gridWithinPolygonGenerator import GridGenerator
+# from GOOGLE.Field.Grid.gridWithinPolygonGenerator import GridGenerator
+from GOOGLE.Field.Grid.HexagonalGrid2D import HexgonalGrid2DGenerator
 from usr_func import *
 
 PATH_OPERATION_AREA = "/Users/yaoling/OneDrive - NTNU/MASCOT_PhD/Projects/GOOGLE/Config/OpArea.csv"
@@ -9,7 +10,8 @@ DISTANCE_LATERAL = 150
 
 polygon = pd.read_csv(PATH_OPERATION_AREA).to_numpy()
 munkholmen = pd.read_csv(PATH_MUNKHOLMEN).to_numpy()
-gridGenerator = GridGenerator(polygon=polygon, depth=[0], distance_neighbour=DISTANCE_LATERAL, no_children=6, points_allowed=5000)
+gridGenerator = HexgonalGrid2DGenerator(polygon_within=polygon, polygon_without=munkholmen, neighbour_distance=500)
+# gridGenerator = GridGenerator(polygon=polygon, depth=[0], distance_neighbour=DISTANCE_LATERAL, no_children=6, points_allowed=5000)
 # grid = gridGenerator.grid
 coordinates = gridGenerator.coordinates
 
@@ -17,4 +19,6 @@ plt.plot(coordinates[:, 1], coordinates[:, 0], 'k.')
 plt.plot(polygon[:, 1], polygon[:, 0], 'r-.')
 plt.plot(munkholmen[:, 1], munkholmen[:, 0], 'r-.')
 plt.show()
+
+
 
