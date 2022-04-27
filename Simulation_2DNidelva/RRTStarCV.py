d3e42f43b16bba7350f788bf5607740f609db072
Yaolin Ge <geyaolin@gmail.com>
2022-04-27 15:53:28 +0200

    update_operation_area

--- Simulation_2DNidelva/RRTStarCV.py
+++ Simulation_2DNidelva/RRTStarCV.py
@@ -72,36 +72,39 @@ class RRTStarCV:
         self.tree_table[-1, :] = [x_target, y_target, np.inf, FAIL_TO_GENERATE_PATH]
         self.goal_indices = self.goal_random_indices[ind_selected]
 
-        # Ntest = MAX_ITERATION-1
-        # for i in range(1, Ntest):
+        Ntest = MAX_ITERATION-1
+        for i in range(1, Ntest):
         # plt.plot(self.tree_table[:Ntest, 1], self.tree_table[:Ntest, 0], 'bx')
-        for i in range(1, MAX_ITERATION-1):
+        # for i in range(1, MAX_ITERATION-1):
             if self.goal_indices[i] < GOAL_SAMPLE_RATE:
                 self.tree_table[i, :] = [x_target, y_target, 0, 0] # refresh table
 
+            # get nearest neighbour
             dx = (self.tree_table[:i, 0] - self.tree_table[i, 0]) ** 2
             dy = (self.tree_table[:i, 1] - self.tree_table[i, 1]) ** 2
             dd = dx + dy
             ind_nearest = np.argmin(dd)
             self.tree_table[i, 3] = ind_nearest
 
-            dx1 = (self.tree_table[ind_nearest, 0] - self.tree_table[i, 0]) ** 2
-            dy1 = (self.tree_table[ind_nearest, 1] - self.tree_table[i, 1]) ** 2
+            # steer location to be witin stepsize
+            x0 = self.tree_table[ind_nearest, 0]
+            y0 = self.tree_table[ind_nearest, 1]
+            x1 = self.tree_table[i, 0]
+            y1 = self.tree_table[i, 1]
+
+            dx1 = (x1 - x0) ** 2
+            dy1 = (y1 - y0) ** 2
             dd1 = np.sqrt(dx1 + dy1)
             if dd1 > STEPSIZE:
-                angle = np.math.atan2(self.tree_table[i, 0] - self.tree_table[ind_nearest, 0],
-                                      self.tree_table[i, 1] - self.tree_table[ind_nearest, 1])
-                self.tree_table[i, 1] = self.tree_table[ind_nearest, 1] + STEPSIZE * np.cos(angle)
-                self.tree_table[i, 0] = self.tree_table[ind_nearest, 0] + STEPSIZE * np.sin(angle)
+                angle = np.math.atan2(x1 - x0, y1 - y0)
+                self.tree_table[i, 1] = y1 + STEPSIZE * np.cos(angle)
+                self.tree_table[i, 0] = x1 + STEPSIZE * np.sin(angle)
             if self.is_location_legal(self.tree_table[i, 0], self.tree_table[i, 1]):
-                x0 = self.tree_table[ind_nearest, 0]
-                y0 = self.tree_table[ind_nearest, 1]
-                x1 = self.tree_table[i, 0]
-                y1 = self.tree_table[i, 1]
                 self.tree_table[i, 2] = self.get_cost_along_path(x0, y0, x1, y1, self.tree_table[ind_nearest, 2])
             else:
                 self.tree_table[i, 2] = np.inf
 
+            # rewire te tree
             dx2 = (self.tree_table[:i, 0] - self.tree_table[i, 0]) ** 2
             dy2 = (self.tree_table[:i, 1] - self.tree_table[i, 1]) ** 2
             dd2 = np.sqrt(dx2 + dy2)
@@ -126,11 +129,11 @@ class RRTStarCV:
                     self.tree_table[i, 2] = cost_path2
 
             for ind_neighbour in ind_neighbours:
-                x1 = self.tree_table[i, 0]
-                y1 = self.tree_table[i, 1]
-                x2 = self.tree_table[ind_neighbour, 0]
-                y2 = self.tree_table[ind_neighbour, 1]
-                cost_path1 = self.get_cost_along_path(x1, y1, x2, y2, self.tree_table[i, 2])
+                x5 = self.tree_table[i, 0]
+                y5 = self.tree_table[i, 1]
+                x6 = self.tree_table[ind_neighbour, 0]
+                y6 = self.tree_table[ind_neighbour, 1]
+                cost_path1 = self.get_cost_along_path(x5, y5, x6, y6, self.tree_table[i, 2])
 
                 if cost_path1 < self.tree_table[ind_neighbour, 2]:
 
@@ -143,51 +146,69 @@ class RRTStarCV:
             if dd3 <= HOME_RADIUS:
                 self.tree_table[-1, 3] = i
 
-        path_to_target = []
-        path_to_target.append([x_target, y_target])
+            plt.figure()
+            x11 = self.tree_table[i, 0]
+            y11 = self.tree_table[i, 1]
+            x22 = self.tree_table[int(self.tree_table[i, 3]), 0]
+            y22 = self.tree_table[int(self.tree_table[i, 3]), 1]
+            plt.plot([y11, y22], [x11, x22], 'g-')
+            plt.plot(y_current, x_current, 'bs')
+            plt.plot(y_target, x_target, 'r*')
+            plt.xlim([np.min(self.polygon_border[:, 1]), np.max(self.polygon_border[:, 1])])
+            plt.ylim([np.min(self.polygon_border[:, 0]), np.max(self.polygon_border[:, 0])])
+            plt.plot(self.polygon_obstacle[:, 1], self.polygon_obstacle[:, 0], 'r-.')
+            plt.plot(self.polygon_border[:, 1], self.polygon_border[:, 0], 'r-.')
+            plt.grid()
+            plt.savefig(FILEPATH + "fig/tree/P_{:03d}.jpg".format(i))
+            plt.close("all")
+            print(i)
+
+
+        self.path_to_target = []
+        self.path_to_target.append([x_target, y_target])
         if self.tree_table[-1, 3] != FAIL_TO_GENERATE_PATH:
             ind_pointer = int(self.tree_table[-1, 3])
             while self.tree_table[ind_pointer, 3] != 0:
                 ind = int(self.tree_table[ind_pointer, 3])
-                path_to_target.append([self.tree_table[ind, 0], self.tree_table[ind, 1]])
+                self.path_to_target.append([self.tree_table[ind, 0], self.tree_table[ind, 1]])
                 ind_pointer = ind
         else:
-            path_to_target.append([x_current, y_current])
-        path_to_target = np.flipud(np.array(path_to_target))
+            self.path_to_target.append([x_current, y_current])
+        self.path_to_target = np.flipud(np.array(self.path_to_target))
 
-        dx4 = (path_to_target[1, 0] - x_current)**2
-        dy4 = (path_to_target[1, 1] - y_current)**2
+        dx4 = (self.path_to_target[1, 0] - x_current)**2
+        dy4 = (self.path_to_target[1, 1] - y_current)**2
         dd4 = np.sqrt(dx4 + dy4)
 
         if dd4 > STEPSIZE:
-            angle = np.math.atan2(path_to_target[1, 0] - x_current,
-                                  path_to_target[1, 1] - y_current)
+            angle = np.math.atan2(self.path_to_target[1, 0] - x_current,
+                                  self.path_to_target[1, 1] - y_current)
             self.y_next = y_current + STEPSIZE * np.cos(angle)
             self.x_next = x_current + STEPSIZE * np.sin(angle)
         else:
-            self.x_next = path_to_target[1, 0]
-            self.y_next = path_to_target[1, 1]
+            self.x_next = self.path_to_target[1, 0]
+            self.y_next = self.path_to_target[1, 1]
 
         t2 = time.time()
         print("RRTStarCV takes: ", t2 - t1)
-        # for i in range(Ntest):
-        #     x1 = self.tree_table[i, 0]
-        #     y1 = self.tree_table[i, 1]
-        #     x2 = self.tree_table[int(self.tree_table[i, 3]), 0]
-        #     y2 = self.tree_table[int(self.tree_table[i, 3]), 1]
-        #     plt.plot([y1, y2], [x1, x2], 'g-')
-        #
-        # plt.plot(path_to_target[:, 1], path_to_target[:, 0], 'r')
-        # # print(x_target, y_target)
-        # # plt.plot(y_target, x_target, 'ks', ms=20)
-        # from matplotlib.cm import get_cmap
-        # # plt.plot(self.tree_table[:Ntest, 1], self.tree_table[:Ntest, 0], 'gx', ms=2)
-        # plt.scatter(self.tree_table[:Ntest, 1], self.tree_table[:Ntest, 0], c=self.tree_table[:Ntest, 2],
-        #             cmap=get_cmap('RdBu', 2), vmin=0, vmax=5)
-        # plt.plot(self.polygon_obstacle[:, 1], self.polygon_obstacle[:, 0], 'r-.')
-        # plt.plot(self.polygon_border[:, 1], self.polygon_border[:, 0], 'r-.')
-        # plt.plot(y_target, x_target, 'r*', ms=10)
-        # plt.colorbar()
+        for i in range(Ntest):
+            x1 = self.tree_table[i, 0]
+            y1 = self.tree_table[i, 1]
+            x2 = self.tree_table[int(self.tree_table[i, 3]), 0]
+            y2 = self.tree_table[int(self.tree_table[i, 3]), 1]
+            plt.plot([y1, y2], [x1, x2], 'g-')
+
+        plt.plot(self.path_to_target[:, 1], self.path_to_target[:, 0], 'r')
+        # print(x_target, y_target)
+        # plt.plot(y_target, x_target, 'ks', ms=20)
+        from matplotlib.cm import get_cmap
+        # plt.plot(self.tree_table[:Ntest, 1], self.tree_table[:Ntest, 0], 'gx', ms=2)
+        plt.scatter(self.tree_table[:Ntest, 1], self.tree_table[:Ntest, 0], c=self.tree_table[:Ntest, 2],
+                    cmap=get_cmap('RdBu', 2), vmin=0, vmax=5)
+        plt.plot(self.polygon_obstacle[:, 1], self.polygon_obstacle[:, 0], 'r-.')
+        plt.plot(self.polygon_border[:, 1], self.polygon_border[:, 0], 'r-.')
+        plt.plot(y_target, x_target, 'r*', ms=10)
+        plt.colorbar()
         # pass
 
     def is_location_legal(self, x, y):
@@ -264,6 +285,9 @@ class RRTStarCV:
         self.vertices = ellipse.get_verts()
 
         plt.gca().add_patch(ellipse)
+        plt.xlim([np.min(self.polygon_border[:, 1]), np.max(self.polygon_border[:, 1])])
+        plt.ylim([np.min(self.polygon_border[:, 0]), np.max(self.polygon_border[:, 0])])
+        plt.grid()
         plt.show()
 
         # plt.plot(self.random_locations[:, 1], self.random_locations[:, 0], 'r.', alpha=.05)
@@ -272,9 +296,14 @@ class RRTStarCV:
 
 if __name__ == "__main__":
     r = RRTStarCV()
-    # r.check()
+    r.check()
 
-
+#%%
+# x1 = 1000
+# y1 = -2000
+# x2 = 4000
+# y2 = 0
+# print(r.is_path_legal(x1, y1, x2, y2))
 
 
 
