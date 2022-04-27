c78bb190f5400213af7f83783f0d46fcec9d2c62
Yaolin Ge <geyaolin@gmail.com>
2022-04-27 15:53:28 +0200

    update_operation_area

--- Simulation_2DNidelva/SimulatorTest.py
+++ Simulation_2DNidelva/SimulatorTest.py
@@ -4,6 +4,7 @@ Author: Yaolin Ge
 Contact: yaolin.ge@ntnu.no
 Date: 2022-04-27
 """
+import matplotlib.pyplot as plt
 
 from usr_func import *
 from GOOGLE.Simulation_2DNidelva.Config.Config import *
@@ -13,10 +14,10 @@ from GOOGLE.Simulation_2DNidelva.RRTStarCV import RRTStarCV
 
 
 # == Set up
-LAT_START = 63.448747
-LON_START = 10.416038
+LAT_START = 63.450912
+LON_START = 10.431774
 X_START, Y_START = latlon2xy(LAT_START, LON_START, LATITUDE_ORIGIN, LONGITUDE_ORIGIN)
-NUM_STEPS = 10
+NUM_STEPS = 50
 # ==
 
 
@@ -44,9 +45,13 @@ class Simulator:
         y_current = Y_START
         x_previous = x_current
         y_previous = y_current
-        for i in range(NUM_STEPS):
-            print("Step: ", i)
+        trajectory = []
+
+        for j in range(NUM_STEPS):
+            print("Step: ", j)
             print("x_next, y_next", x_current, y_current)
+            trajectory.append([x_current, y_current])
+
             ind_measured = self.grf_model.get_ind_from_location(x_current, y_current)
             self.grf_model.update_grf_model(ind_measured, self.grf_model.mu_truth[ind_measured])
             mu = self.grf_model.mu_cond
@@ -56,11 +61,77 @@ class Simulator:
                                                 self.CV.budget.line_budget_ellipse, x_current, y_current)
             x_next = self.rrtstar.x_next
             y_next = self.rrtstar.y_next
+
+
+            plt.figure(figsize=(10, 10))
+            fig, ax = plt.subplots(1, 1)
+            ax.plot(self.rrtstar.polygon_border[:, 1], self.rrtstar.polygon_border[:, 0], 'k-.')
+            ax.plot(self.rrtstar.polygon_obstacle[:, 1], self.rrtstar.polygon_obstacle[:, 0], 'k-.')
+
+
+            for i in range(self.rrtstar.tree_table.shape[0]):
+                x1 = self.rrtstar.tree_table[i, 0]
+                y1 = self.rrtstar.tree_table[i, 1]
+                x2 = self.rrtstar.tree_table[int(self.rrtstar.tree_table[i, 3]), 0]
+                y2 = self.rrtstar.tree_table[int(self.rrtstar.tree_table[i, 3]), 1]
+                plt.plot([y1, y2], [x1, x2], 'g-')
+            ax.plot(self.rrtstar.path_to_target[:, 1], self.rrtstar.path_to_target[:, 0], 'r')
+
+            # plt.scatter(self.grf_model.grf_grid[:, 1], self.grf_model.grf_grid[:, 0], c=mu, cmap=get_cmap('RdBu', 15),
+            #             vmin=10, vmax=30, alpha=.5)
+
+            xplot = self.grf_model.grf_grid[:, 1]
+            yplot = self.grf_model.grf_grid[:, 0]
+            triangulated = tri.Triangulation(xplot, yplot)
+            x_triangulated = xplot[triangulated.triangles].mean(axis=1)
+            y_triangulated = yplot[triangulated.triangles].mean(axis=1)
+
+            ind_mask = []
+            for i in range(len(x_triangulated)):
+                ind_mask.append(self.is_masked(y_triangulated[i], x_triangulated[i]))
+            triangulated.set_mask(ind_mask)
+            refiner = tri.UniformTriRefiner(triangulated)
+            triangulated_refined, value_refined = refiner.refine_field(mu.flatten(), subdiv=3)
+            contourplot = ax.tricontourf(triangulated_refined, value_refined, cmap=get_cmap("RdBu", 15), alpha=.5)
+            im = ax.tricontour(triangulated_refined, value_refined, vmin=10, vmax=30, alpha=.5)
+            plt.colorbar(im)
+
+            ellipse = Ellipse(xy=(self.CV.budget.y_middle, self.CV.budget.x_middle), width=2 * self.CV.budget.ellipse_a,
+                              height=2 * self.CV.budget.ellipse_b, angle=math.degrees(self.CV.budget.angle),
+                              edgecolor='r', fc='None', lw=2)
+            plt.gca().add_patch(ellipse)
+
+            ax.plot(y_current, x_current, 'bs')
+            ax.plot(y_previous, x_previous, 'y^')
+            ax.plot(y_next, x_next, 'r*')
+            ax.plot(Y_HOME, X_HOME, 'k*')
+
+            p_traj = np.array(trajectory)
+            ax.plot(p_traj[:, 1], p_traj[:, 0], 'k.-')
+            plt.xlim([np.min(self.rrtstar.polygon_border[:, 1]), np.max(self.rrtstar.polygon_border[:, 1])])
+            plt.ylim([np.min(self.rrtstar.polygon_border[:, 0]), np.max(self.rrtstar.polygon_border[:, 0])])
+
+            plt.xlabel("East")
+            plt.ylabel("North")
+
+            plt.title("Updated mean after step: " + str(j))
+            plt.savefig(FILEPATH+"fig/rrtstar/P_{:03d}.jpg".format(j))
+            plt.close("all")
+
+
             x_previous = x_current
             y_previous = y_current
             x_current = x_next
             y_current = y_next
 
+    def is_masked(self, x, y):
+        point = Point(x, y)
+        masked = False
+        if (self.rrtstar.polygon_obstacle_shapely.contains(point) or
+                not self.rrtstar.polygon_border_shapely.contains(point) or
+                not self.rrtstar.polygon_budget_ellipse.contains(point)):
+            masked = True
+        return masked
 
 if __name__ == "__main__":
     s = Simulator()
