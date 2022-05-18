115b7cd341f08a7bc0fc81e82e068365605dff31
Yaolin Ge <geyaolin@gmail.com>
2022-05-18 11:48:47 +0200

    update_EDA

--- Nidelva2D/EDA.py
+++ Nidelva2D/EDA.py
@@ -5,7 +5,9 @@ Contact: yaolin.ge@ntnu.no
 Date: 2022-05-13
 """
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pandas as pd
+from GOOGLE.Nidelva2D.grfar_model import GRFAR
 from GOOGLE.Nidelva2D.Config.Config import FILEPATH, LATITUDE_ORIGIN, LONGITUDE_ORIGIN
 from usr_func import *
 
@@ -21,17 +23,50 @@ class EDA:
 
     def load_auv_data(self):
         self.data = pd.read_csv(DATAPATH+"data_sync.csv").to_numpy()
-        self.lat = self.data[:, 0]
-        self.lon = self.data[:, 1]
-        self.depth = self.data[:, 2]
-        self.salinity = self.data[:, 3]
+        self.timestamp = self.data[:, 0]
+        self.lat = self.data[:, 1]
+        self.lon = self.data[:, 2]
+        self.depth = self.data[:, 3]
+        self.salinity = self.data[:, 4]
+        self.temperature = self.data[:, 5]
         print("AUV data is loaded successfully!")
+
+    def load_grfar_model(self):
+        self.grfar_model = GRFAR()
+        self.grf_grid = self.grfar_model.grf_grid
+        self.N_grf_grid = self.grf_grid.shape[0]
+        print("S2: GRFAR model is loaded successfully!")
+
+    def assimilate_data(self, dataset):
+        print("dataset before filtering: ", dataset[:10, :])
+        depth_dataset = np.abs(dataset[:, 2])
+        ind_selected_depth_layer = np.where((depth_dataset >= .25) * (depth_dataset <= DEPTH_LAYER + .5))[0]
+        dataset = dataset[ind_selected_depth_layer, :]
+        print("dataset after filtering: ", dataset[:10, :])
+        t1 = time.time()
+        dx = (vectorise(dataset[:, 0]) @ np.ones([1, self.N_grf_grid]) -
+              np.ones([dataset.shape[0], 1]) @ vectorise(self.grf_grid[:, 0]).T) ** 2
+        dy = (vectorise(dataset[:, 1]) @ np.ones([1, self.N_grf_grid]) -
+              np.ones([dataset.shape[0], 1]) @ vectorise(self.grf_grid[:, 1]).T) ** 2
+        dist = dx + dy
+        ind_min_distance = np.argmin(dist, axis=1)
+        t2 = time.time()
+        ind_assimilated = np.unique(ind_min_distance)
+        salinity_assimilated = np.zeros(len(ind_assimilated))
+        for i in range(len(ind_assimilated)):
+            ind_selected = np.where(ind_min_distance == ind_assimilated[i])[0]
+            salinity_assimilated[i] = np.mean(dataset[ind_selected, 3])
+        print("Data assimilation takes: ", t2 - t1)
+        self.auv_data = []
+        print("Reset auv_data: ", self.auv_data)
+        return vectorise(ind_assimilated), vectorise(salinity_assimilated)
+
 
     def plot_scatter_data(self):
         fig = go.Figure(data=go.Scatter3d(
-            x=self.data[:, 1],
-            y=self.data[:, 0],
-            z=-self.data[:, 2],
+            x=self.lon,
+            y=self.lat,
+            z=-self.depth,
             mode='markers',
             marker=dict(color=self.data[:, 3], size=10)
         ))
@@ -46,8 +81,9 @@ class EDA:
 
 if __name__ == "__main__":
     e = EDA()
-    e.plot_2d()
-    # e.plot_scatter_data()
+    # e.plot_2d()
+    e.plot_scatter_data()
+
 
 
 
