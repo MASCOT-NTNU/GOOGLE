20208af7f9d2ec373f2470b1ee3058632effa8ac
Yaolin Ge <geyaolin@gmail.com>
2022-05-20 10:57:31 +0200

    update_eda_fig

--- Nidelva2D/EDA/EDA.py
+++ Nidelva2D/EDA/EDA.py
@@ -4,13 +4,19 @@ Author: Yaolin Ge
 Contact: yaolin.ge@ntnu.no
 Date: 2022-05-13
 """
-from GOOGLE.Nidelva2D.grfar_model import GRFAR
+import matplotlib.pyplot as plt
+
+from GOOGLE.Nidelva2D.EDA.grfar_model import GRFAR
 from GOOGLE.Nidelva2D.Config.Config import FILEPATH, LATITUDE_ORIGIN, LONGITUDE_ORIGIN, DEPTH_LAYER
 from usr_func import *
 from DataHandler.SINMOD import SINMOD
+from GOOGLE.Nidelva2D.EDA.plotting_func import plotf_vector
+from GOOGLE.Nidelva2D.EDA.RRTStarCV import RRTStarCV
 
 
 DATAPATH = "/Users/yaolin/OneDrive - NTNU/MASCOT_PhD/Data/Nidelva/20220511/GOOGLE/"
+SINMODPATH = "/Users/yaolin/HomeOffice/GOOGLE/Experiments/20220511/"
+figpath = "/Users/yaolin/HomeOffice/GOOGLE/Experiments/20220511/fig/"
 
 
 class EDA:
@@ -18,6 +24,7 @@ class EDA:
     def __init__(self):
         self.load_auv_data()
         self.load_grfar_model()
+        self.load_rrtstar()
 
     def load_auv_data(self):
         self.data_auv = pd.read_csv(DATAPATH + "data_sync.csv").to_numpy()
@@ -29,12 +36,16 @@ class EDA:
         self.temperature_auv = self.data_auv[:, 5]
         print("AUV data is loaded successfully!")
 
-    def load_sinmod_data(self):
-
-        self.sinmod = SINMOD()
-        self.sinmod.load_sinmod_data(raw_data=True)
-        coordinates_auv = np.vstack((self.lat_auv, self.lon_auv, self.depth_auv)).T
-        self.sinmod.get_data_at_coordinates(coordinates_auv)
+    def load_sinmod_data(self, data_exists=True):
+        if not data_exists:
+            self.sinmod = SINMOD()
+            self.sinmod.load_sinmod_data(raw_data=True)
+            coordinates_auv = np.vstack((self.lat_auv, self.lon_auv, self.depth_auv)).T
+            self.sinmod.get_data_at_coordinates(coordinates_auv)
+        else:
+            self.data_sinmod = pd.read_csv(SINMODPATH+"data_sinmod.csv")
+            print("SINMOD data is loaded successfully!")
+            print(self.data_sinmod.head())
         pass
 
     def load_grfar_model(self):
@@ -42,6 +53,10 @@ class EDA:
         self.grf_grid = self.grfar_model.grf_grid
         self.N_grf_grid = self.grf_grid.shape[0]
         print("S2: GRFAR model is loaded successfully!")
+
+    def load_rrtstar(self):
+        self.rrtstar = RRTStarCV()
+        print("S3: RRTStar is loaded successfully!")
 
     def assimilate_data(self, dataset):
         print("dataset before filtering: ", dataset[:10, :])
@@ -83,24 +98,123 @@ class EDA:
         plt.colorbar()
         plt.show()
 
-    def get_residual_with_sinmod(self):
-        self.sinmod = SINMOD()
-        self.sinmod.load_sinmod_data(raw_data=True)
-        coordinates_auv = np.vstack((self.lat_auv, self.lon_auv, self.depth_auv)).T
-        self.sinmod.get_data_at_coordinates(coordinates_auv)
-        pass
+    def plot_sinmod_on_grf_grid(self):
+        lat_grid, lon_grid = xy2latlon(self.grf_grid[:, 0], self.grf_grid[:, 1], LATITUDE_ORIGIN, LONGITUDE_ORIGIN)
+        lat_p, lon_p = xy2latlon(self.rrtstar.polygon_border[:, 0], self.rrtstar.polygon_border[:, 1], LATITUDE_ORIGIN,
+                                 LONGITUDE_ORIGIN)
+        polygon_border = np.vstack((lat_p, lon_p)).T
+
+        lat_o, lon_o = xy2latlon(self.rrtstar.polygon_obstacle[:, 0], self.rrtstar.polygon_obstacle[:, 1], LATITUDE_ORIGIN,
+                                 LONGITUDE_ORIGIN)
+        polygon_obstacle = np.vstack((lat_o, lon_o)).T
+
+        plt.figure(figsize=(12, 10))
+        plotf_vector(lon=lon_grid, lat=lat_grid, values=self.grfar_model.mu_sinmod,
+                     title="SINMOD on grid on 20220511",
+                     cmap=get_cmap("BrBG", 10), cbar_title="Salinity", vmin=2, vmax=32, stepsize=1, threshold=26.8,
+                     polygon_border=polygon_border, polygon_obstacle=polygon_obstacle, xlabel="Lon [deg]", ylabel="Lat [deg]")
+        plt.savefig(figpath + "sinmod_grid.jpg")
+        plt.show()
+
+    def plot_prior(self):
+        lat_grid, lon_grid = xy2latlon(self.grf_grid[:, 0], self.grf_grid[:, 1], LATITUDE_ORIGIN, LONGITUDE_ORIGIN)
+        lat_p, lon_p = xy2latlon(self.rrtstar.polygon_border[:, 0], self.rrtstar.polygon_border[:, 1], LATITUDE_ORIGIN,
+                                 LONGITUDE_ORIGIN)
+        polygon_border = np.vstack((lat_p, lon_p)).T
+
+        lat_o, lon_o = xy2latlon(self.rrtstar.polygon_obstacle[:, 0], self.rrtstar.polygon_obstacle[:, 1],
+                                 LATITUDE_ORIGIN,
+                                 LONGITUDE_ORIGIN)
+        polygon_obstacle = np.vstack((lat_o, lon_o)).T
+
+        plt.figure(figsize=(12, 10))
+        plotf_vector(lon=lon_grid, lat=lat_grid, values=self.grfar_model.mu_prior,
+                     title="SINMOD on grid on 20220511",
+                     cmap=get_cmap("BrBG", 10), cbar_title="Salinity", vmin=2, vmax=32, stepsize=1, threshold=26.8,
+                     polygon_border=polygon_border, polygon_obstacle=polygon_obstacle, xlabel="Lon [deg]",
+                     ylabel="Lat [deg]")
+        plt.savefig(figpath + "prior.jpg")
+        plt.show()
 
 if __name__ == "__main__":
     e = EDA()
-    e.plot_2d()
+    e.load_sinmod_data(data_exists=True)
+    e.plot_prior()
+    # e.plot_sinmod_on_grf_grid()
+    # e.plot_2d()
     # e.get_residual_with_sinmod()
     # e.plot_scatter_data()
 
+#%%
+plt.scatter(e.grf_grid[:, 1], e.grf_grid[:, 0], c=e.grfar_model.mu_prior, cmap=get_cmap("BrBG", 10), vmin=10, vmax=27)
+plt.colorbar()
+plt.show()
+
+#%%
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import plotly
+import netCDF4
+from datetime import datetime
+from matplotlib.cm import get_cmap
+import re
+
+LATITUDE_ORIGIN = 63.4269097
+LONGITUDE_ORIGIN = 10.3969375
+CIRCUMFERENCE = 40075000 # [m], circumference
+circumference = CIRCUMFERENCE
+def latlon2xy(lat, lon, lat_origin, lon_origin):
+    x = np.deg2rad((lat - lat_origin)) / 2 / np.pi * CIRCUMFERENCE
+    y = np.deg2rad((lon - lon_origin)) / 2 / np.pi * CIRCUMFERENCE * np.cos(np.deg2rad(lat))
+    return x, y
+
+file = "/Users/yaolin/Library/CloudStorage/OneDrive-NTNU/MASCOT_PhD/Data/Nidelva/SINMOD_DATA/samples_2022.05.11.nc"
+figpath = "/Users/yaolin/HomeOffice/GOOGLE/Experiments/20220511/fig/"
+# file = "/Users/yaolin/OneDrive - NTNU/MASCOT_PhD/Data/Nidelva/SINMOD_DATA/samples_2022.05.10.nc"
+# figpath = "/Users/yaolin/HomeOffice/GOOGLE/Experiments/20220509/fig/"
+
+sinmod = netCDF4.Dataset(file)
+ind_before = re.search("samples_", file)
+ind_after = re.search(".nc", file)
+date_string = file[ind_before.end():ind_after.start()]
+ref_timestamp = datetime.strptime(date_string, "%Y.%m.%d").timestamp()
+timestamp = np.array(sinmod["time"]) * 24 * 3600 + ref_timestamp #change ref timestamp
+lat_sinmod = np.array(sinmod['gridLats'])
+lon_sinmod = np.array(sinmod['gridLons'])
+depth_sinmod = np.array(sinmod['zc'])
+salinity_sinmod = np.array(sinmod['salinity'])
+
+# for i in range(salinity_sinmod.shape[0]):
+#     print(i)
+#     plt.figure(figsize=(12, 10))
+#     plt.scatter(lon_sinmod, lat_sinmod, c=salinity_sinmod[i, 0, :, :], cmap=get_cmap("BrBG", 8), vmin=10, vmax=26.8)
+#     plt.xlabel("Lon [deg]")
+#     plt.ylabel("Lat [deg]")
+#     plt.title("SINMOD Surface Salinity Estimation on " + datetime.fromtimestamp(timestamp[i]).strftime("%H:%M, %Y-%m-%d"))
+#     plt.colorbar()
+#     plt.savefig(figpath+"sinmod/P_{:03d}.jpg".format(i))
+#     plt.close("all")
+
+#%%
+lat_grid, lon_grid = xy2latlon(e.grf_grid[:, 0], e.grf_grid[:, 1], LATITUDE_ORIGIN, LONGITUDE_ORIGIN)
+sal_mean = np.mean(salinity_sinmod[:, 0, :, :], axis=0)
+plt.figure(figsize=(12, 10))
+# plt.scatter(lon_sinmod, lat_sinmod, c=sal_mean, cmap=get_cmap("BrBG", 8), vmin=10, vmax=26.8)
+plt.xlabel("Lon [deg]")
+plt.ylabel("Lat [deg]")
+
+plt.scatter(lon_grid, lat_grid, c=e.grfar_model.mu_sinmod, cmap=get_cmap("BrBG", 8), vmin=10, vmax=26.8)
+# plt.plot(lon_grid, lat_grid, 'y.', alpha=.3)
+plt.colorbar()
+plt.title("SINMOD" + datetime.fromtimestamp(timestamp[0]).strftime("%Y-%m-%d"))
+plt.savefig(figpath+"sinmod_grid.jpg")
+plt.show()
 
 
-
-
-
+#
 
 
 
---
+++ Nidelva2D/EDA/plotting_func.py
@@ -0,0 +1,134 @@
+"""
+This script contains all the necessary plotting functions
+Author: Yaolin Ge
+Contact: yaolin.ge@ntnu.no
+Date: 2022-03-07
+"""
+import numpy as np
+
+from usr_func import *
+from GOOGLE.Nidelva2D.Config.Config import *
+
+
+def plotf_vector(lon, lat, values, title=None, alpha=None, cmap=get_cmap("BrBG", 10), cbar_title='test', colorbar=True,
+                 vmin=None, vmax=None, ticks=None, stepsize=None, threshold=None, polygon_border=None, polygon_obstacle=None,
+                 xlabel=None, ylabel=None):
+
+    triangulated = tri.Triangulation(lon, lat)
+    lon_triangulated = lon[triangulated.triangles].mean(axis=1)
+    lat_triangulated = lat[triangulated.triangles].mean(axis=1)
+
+    ind_mask = []
+    for i in range(len(lon_triangulated)):
+        ind_mask.append(is_masked(lat_triangulated[i], lon_triangulated[i], Polygon(polygon_border),
+                                  Polygon(polygon_obstacle)))
+    triangulated.set_mask(ind_mask)
+    refiner = tri.UniformTriRefiner(triangulated)
+    triangulated_refined, value_refined = refiner.refine_field(values.flatten(), subdiv=3)
+
+    ax = plt.gca()
+    # ax.triplot(triangulated, lw=0.5, color='white')
+    if np.any([vmin, vmax]):
+        levels = np.arange(vmin, vmax, stepsize)
+    else:
+        levels = None
+
+    print("levels: ", levels)
+
+    if np.any(levels):
+        linewidths = np.ones_like(levels) * .3
+        colors = len(levels) * ['black']
+        if threshold:
+            dist = np.abs(threshold - levels)
+            ind = np.where(dist == np.amin(dist))[0]
+            linewidths[ind] = 3
+            colors[ind[0]] = 'red'
+        contourplot = ax.tricontourf(triangulated_refined, value_refined, levels=levels, cmap=cmap, alpha=alpha)
+        ax.tricontour(triangulated_refined, value_refined, levels=levels, linewidths=linewidths, colors=colors,
+                      alpha=alpha)
+    else:
+        contourplot = ax.tricontourf(triangulated_refined, value_refined, cmap=cmap, alpha=alpha)
+        ax.tricontour(triangulated_refined, value_refined, vmin=vmin, vmax=vmax, alpha=alpha)
+
+    if colorbar:
+        cbar = plt.colorbar(contourplot, ax=ax, ticks=ticks)
+        cbar.ax.set_title(cbar_title)
+    # plt.plot(knowledge.polygon_border_xy[:, 1], knowledge.polygon_border_xy[:, 0], 'k-', linewidth=1)
+    # plt.plot(knowledge.polygon_obstacle_xy[:, 1], knowledge.polygon_obstacle_xy[:, 0], 'k-', linewidth=1)
+    # plt.plot(knowledge.starting_location.y, knowledge.starting_location.x, 'kv', ms=10)
+    # plt.plot(knowledge.goal_location.y, knowledge.goal_location.x, 'rv', ms=10)
+    plt.xlim([np.amin(lon), np.amax(lon)])
+    plt.ylim([np.amin(lat), np.amax(lat)])
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot(polygon_border[:, 1], polygon_border[:, 0], 'k-.', lw=2)
+    plt.plot(polygon_obstacle[:, 1], polygon_obstacle[:, 0], 'k-.', lw=2)
+
+    # plt.show()
+
+
+def plotf_vector_scatter(grid, values, title=None, alpha=None, cmap="Paired", cbar_title='test', colorbar=True,
+                 vmin=None, vmax=None, ticks=None):
+    # x = grid[:, 0]
+    # y = grid[:, 1]
+    # nx = 100
+    # ny = 100
+    #
+    # xmin, ymin = map(np.amin, [x, y])
+    # xmax, ymax = map(np.amax, [x, y])
+    #
+    # xv = np.linspace(xmin, xmax, nx)
+    # yv = np.linspace(ymin, ymax, ny)
+    # grid_x, grid_y = np.meshgrid(xv, yv)
+    #
+    # grid_values = griddata(grid, values, (grid_x, grid_y))
+    # plt.figure()
+    plt.scatter(grid[:, 1], grid[:, 0], c=values, cmap=cmap, alpha=alpha, vmin=vmin, vmax=vmax)
+    # plt.scatter(grid_x, grid_y, c=grid_values, cmap=cmap, alpha=alpha, vmin=vmin, vmax=vmax)
+    # plt.xlim(XLIM)
+    # plt.ylim(YLIM)
+    if colorbar:
+        cbar = plt.colorbar(ticks=ticks)
+        cbar.ax.set_title(cbar_title)
+    plt.title(title)
+    # plt.show()
+
+
+def is_masked(lat, lon, polygon_border=None, polygon_obstacle=None):
+    point = Point(lat, lon)
+    masked = False
+    if polygon_obstacle.contains(point) or not polygon_border.contains(point):
+        masked = True
+    return masked
+
+
+def plotf_budget_radar(centre, radius):
+    plt.plot(centre[0], centre[1], 'yp')
+    radar = plt.Circle((centre[0], centre[1]), radius, color='b', fill=False)
+    plt.gca().add_patch(radar)
+    if radius >= 1:
+        plt.xlim([radius * XLIM[0], radius * XLIM[1]])
+        plt.ylim([radius * YLIM[0], radius * YLIM[1]])
+
+
+def plotf_trajectory(trajectory):
+    path = []
+    for location in trajectory:
+        path.append([location.y, location.x])
+    path = np.array(path)
+    plt.plot(path[:, 0], path[:, 1], 'y.-')
+    plt.plot(path[:, 0], path[:, 1], 'y-')
+
+
+def plotf_matrix(values, title):
+    # grid_values = griddata(grid, values, (grid_x, grid_y))
+    plt.figure()
+    plt.imshow(values, cmap="Paired", extent=(XLIM[0], XLIM[1], YLIM[0], YLIM[1]), origin="lower")
+    plt.colorbar()
+    plt.title(title)
+    plt.show()
+
+
+
+
