8b90c52a14c3e1873d0e148b0adaee97820e63f1
Yaolin Ge <geyaolin@gmail.com>
2022-11-24 12:09:03 +0100

    update_simulation-env

--- Publication/src/Config.py
+++ Publication/src/Config.py
@@ -1,13 +1,9 @@
 """
-Config handles the most important parameter setting in the long horizon operation in MASCOT Porto mission 2022.
-- misssion date: "2022-10-01_2022-10-02"
-- wind direction: ["North", "East", "South", "West"]
-- wind level: ["Mild", "Moderate", "Heavy"], the corresponding wind speed: [0, 2.5, 6] m/s
-- clock_start: 10, when does the operation start: 10 o'clock.
-- clock_end: 16, when does the operation end: 16 o'clock.
-
+Config has the most important parameter setting in the long horizon operation in MASCOT Nidelva mission 2022.
 - polygon_operational_area: the polygon used to define the safe operational area.
 - polygon_operational_area_shapely: shapely object to detect collision or border.
+- polygon_obstalce: polygons used for identifying collisions.
+- polygon_obstalce_shapely: shapely object to detect collision with obstacles.
 
 - starting location: (lat, lon) used to define the starting location for the long horizon operation.
 - home location: (lat, lon) used to define the end location for the home in the long horizon operation.
@@ -22,50 +18,20 @@ class Config:
     """ Config contains essential setup for the simulation or experiment study. """
 
     """ Operational Area. """
-    __polygon_operational_area = pd.read_csv("OPA_GOOGLE_reduced.csv").to_numpy()
+    __polygon_operational_area = pd.read_csv("csv/polygon_border.csv").to_numpy()
     __polygon_operational_area_shapely = Polygon(__polygon_operational_area)
 
     """ Obstacles"""
-    __polygon_obstacles = [np.array([[.4, .4],
-                                     [.6, .5],
-                                     [.5, .6],
-                                     [.3, .4]])]
+    __polygon_obstacle = pd.read_csv("csv/polygon_obstacle.csv").to_numpy()
+    __polygon_obstacle_shapely = Polygon(__polygon_obstacle)
 
     """ Starting and end locations. """
     # c4: start at home
-    __lat_start = 41.12677
-    __lon_start = -8.68574
+    __lat_start = 63.45582
+    __lon_start = 10.43287
 
-    __lat_home = 41.12677
-    __lon_home = -8.68574
-
-    """ Create a resume state. """
-    __resume = False
-
-    @staticmethod
-    def set_mission_date(value: str) -> None:
-        """ Set mission date with a format 2022-10-01_2022-10-02. """
-        Config.__mission_date = value
-
-    @staticmethod
-    def set_wind_direction(value: str) -> None:
-        """ Set wind direction to be North, East, South, West. """
-        Config.__wind_dir = value
-
-    @staticmethod
-    def set_wind_level(value: str) -> None:
-        """ Set wind level to be Mild, Moderate, Heavy. """
-        Config.__wind_level = value
-
-    @staticmethod
-    def set_clock_start(value: int) -> None:
-        """ Set starting clock to be 0, 1, 2, 3, ..., 24. """
-        Config.__clock_start = value
-
-    @staticmethod
-    def set_clock_end(value: int) -> None:
-        """ Set starting clock to be 0, 1, 2, 3, ..., 24. Must be larger than MOHID.__clock_start. """
-        Config.__clock_end = value
+    __lat_home = 63.440323
+    __lon_home = 10.355410
 
     @staticmethod
     def set_polygon_operational_area(value: np.ndarray) -> None:
