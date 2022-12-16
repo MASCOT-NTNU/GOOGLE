f998f405dbf808f4e591a2d85bbeca53d27c3957
Yaolin Ge <geyaolin@gmail.com>
2022-12-16 12:31:19 +0100

    fix_bugs_for_simulation

--- Publication/src/Simulators/Simulator.py
+++ Publication/src/Simulators/Simulator.py
@@ -36,142 +36,128 @@ class Simulator:
         # s2: set up ground truth.
         self.ctd = CTD(self.loc_start)
 
-    def set_weights(self, weight_eibv: float = 1., weight_ivr: float = 1.):
-        if weight_eibv > weight_ivr:
-            self.case = "EIBV"
-        elif weight_eibv < weight_ivr:
-            self.case = "IVR"
-        else:
-            self.case = "EQUAL"
-
-        # s0: set the planner according to their weight set.
-        self.planner = Planner(self.loc_start)
-        self.log = Log(ctd=self.ctd)
-        self.rrtstar = self.planner.get_rrtstarcv()
-        self.cv = self.rrtstar.get_CostValley()
-        self.cv.set_weight_eibv(weight_eibv)
-        self.cv.set_weight_ivr(weight_ivr)
-        self.cv.update_cost_valley()
-        self.grf = self.cv.get_grf_model()
-
-        if self.debug:
-            self.field = self.cv.get_field()
-            self.grid = self.field.get_grid()
-            self.polygon_border = self.config.get_polygon_border()
-            self.polygon_obstacle = self.config.get_polygon_obstacle()
-            self.figpath = os.getcwd() + "/../../fig/Sim_2DNidelva/Simulator/" + self.case + "/"
-            checkfolder(self.figpath)
-
-            plt.figure(figsize=(15, 12))
-            plotf_vector(self.grid[:, 1], self.grid[:, 0], self.grf.get_mu(), xlabel='East', ylabel='North',
-                         title='Prior', cbar_title="Salinity", cmap=get_cmap("RdBu", 10), vmin=10, vmax=33, stepsize=1.5)
-            plt.plot(self.polygon_border[:, 1], self.polygon_border[:, 0], 'r-.')
-            plt.plot(self.polygon_obstacle[:, 1], self.polygon_obstacle[:, 0], 'r-.')
-            plt.xlabel("East")
-            plt.ylabel("North")
-            plt.savefig(self.figpath + "Prior_" + self.case + ".png")
-            plt.show()
-
-            plt.figure(figsize=(15, 12))
-            plotf_vector(self.grid[:, 1], self.grid[:, 0], self.ctd.get_ground_truth(), xlabel='East', ylabel='North',
-                         title='GroundTruth', cbar_title="Salinity", cmap=get_cmap("RdBu", 10), vmin=10, vmax=33,
-                         stepsize=1.5)
-            plt.plot(self.polygon_border[:, 1], self.polygon_border[:, 0], 'r-.')
-            plt.plot(self.polygon_obstacle[:, 1], self.polygon_obstacle[:, 0], 'r-.')
-            plt.xlabel("East")
-            plt.ylabel("North")
-            plt.savefig(self.figpath + "Truth_" + self.case + ".png")
-            plt.show()
-
     def run_simulator(self, num_steps: int = 5) -> tuple:
         """
         Run the autonomous operation according to Sense, Plan, Act philosophy.
         """
-        trajectory = np.empty([0, 2])
-        trajectory = np.append(trajectory, self.loc_start.reshape(1, -1), axis=0)
-        self.log.append_log(self.grf)
+        w_eibv = [1.9, 1., .1]
+        w_ivr = [.1, 1., 1.9]
 
-        t1 = time()
+        traj_sim = np.empty([0, num_steps+1, 2])
+        ibv = np.empty([0, num_steps+1])
+        rmse = np.empty([0, num_steps+1])
+        vr = np.empty([0, num_steps+1])
 
-        """ Debug section. """
-        # rmse = []
-        # from sklearn.metrics import mean_squared_error
-        # mu_truth = self.ctd.get_ground_truth()
-        # wps = np.array([[2000, -2000],
-        #                 [2200, -1800],
-        #                 [2400, -1600],
-        #                 [2600, -1400],
-        #                 [2800, -1200],
-                        # [3000, -1000],
-                        # [2900, -1100],
-                        # [2800, -1300],
-                        # [2700, -1200],
-                        # [1500, -800]
-                        # ])
-        """ End """
+        for k in range(len(w_eibv)):
 
-        for i in range(num_steps):
-        # for i in range(len(wps)):
-            t2 = time()
-            print("Step: ", i, " takes ", t2 - t1, " seconds. ")
-            t1 = time()
+            weight_eibv = w_eibv[k]
+            weight_ivr = w_ivr[k]
+            print("Weight_EIBV: ", weight_eibv, "Weight_IVR: ", weight_ivr)
 
-            """ plotting seciton. """
+            if weight_eibv > weight_ivr:
+                case = "EIBV"
+            elif weight_eibv < weight_ivr:
+                case = "IVR"
+            else:
+                case = "EQUAL"
+
+            # s0: set the planner according to their weight set.
+            planner = Planner(self.loc_start)
+            log = Log(ctd=self.ctd)
+            rrtstar = planner.get_rrtstarcv()
+            cv = rrtstar.get_CostValley()
+            cv.set_weight_eibv(weight_eibv)
+            cv.set_weight_ivr(weight_ivr)
+            cv.update_cost_valley()
+            grf = cv.get_grf_model()
+
             if self.debug:
+                field = cv.get_field()
+                grid = field.get_grid()
+                polygon_border = self.config.get_polygon_border()
+                polygon_obstacle = self.config.get_polygon_obstacle()
+                figpath = os.getcwd() + "/../../fig/Sim_2DNidelva/Simulator/" + case + "/"
+                checkfolder(figpath)
+
                 plt.figure(figsize=(15, 12))
-                cost_valley = self.cv.get_cost_field()
-                plotf_vector(self.grid[:, 1], self.grid[:, 0], cost_valley, xlabel='East', ylabel='North', title='RRTCV',
-                             cbar_title="Cost", cmap=get_cmap("RdBu", 10), vmin=0, vmax=2.2, stepsize=.25)
-                if len(trajectory) > 0:
-                    plt.plot(trajectory[:, 1], trajectory[:, 0], 'k.-')
-                plt.plot(self.polygon_border[:, 1], self.polygon_border[:, 0], 'r-.')
-                plt.plot(self.polygon_obstacle[:, 1], self.polygon_obstacle[:, 0], 'r-.')
+                plotf_vector(grid[:, 1], grid[:, 0], grf.get_mu(), xlabel='East', ylabel='North',
+                             title='Prior', cbar_title="Salinity", cmap=get_cmap("RdBu", 10), vmin=10, vmax=33, stepsize=1.5)
+                plt.plot(polygon_border[:, 1], polygon_border[:, 0], 'r-.')
+                plt.plot(polygon_obstacle[:, 1], polygon_obstacle[:, 0], 'r-.')
                 plt.xlabel("East")
                 plt.ylabel("North")
-                plt.savefig(self.figpath + "P_{:03d}.png".format(i))
-                plt.close("all")
+                plt.savefig(figpath + "Prior_" + case + ".png")
+                plt.show()
 
-            self.planner.update_planning_trackers()
+                plt.figure(figsize=(15, 12))
+                plotf_vector(grid[:, 1], grid[:, 0], self.ctd.get_ground_truth(), xlabel='East', ylabel='North',
+                             title='GroundTruth', cbar_title="Salinity", cmap=get_cmap("RdBu", 10), vmin=10, vmax=33,
+                             stepsize=1.5)
+                plt.plot(polygon_border[:, 1], polygon_border[:, 0], 'r-.')
+                plt.plot(polygon_obstacle[:, 1], polygon_obstacle[:, 0], 'r-.')
+                plt.xlabel("East")
+                plt.ylabel("North")
+                plt.savefig(figpath + "Truth_" + case + ".png")
+                plt.show()
 
-            """ Debug check rmse """
-            # rmse.append(mean_squared_error(mu_truth, self.grf.get_mu(), squared=False))
-            # wp_now = wps[i, :]
-            # ctdd = self.ctd.get_ctd_data(wp_now)
-            # print("CTD: ", ctdd)
-            # self.grf.assimilate_data(ctdd)
-            """ End """
+            # start logging the data.
+            trajectory = np.empty([0, 2])
+            trajectory = np.append(trajectory, self.loc_start.reshape(1, -1), axis=0)
+            log.append_log(grf)
+            print("RMSE: ", log.rmse)
 
-            # p1: parallel move AUV to the first location
-            wp_now = self.planner.get_current_waypoint()
-            trajectory = np.append(trajectory, wp_now.reshape(1, -1), axis=0)
-
-            # s2: obtain CTD data
-            ctd_data = self.ctd.get_ctd_data(wp_now)
-
-            # s3: update pioneer waypoint
             t1 = time()
-            # print("CTD: ", ctd_data)
-            self.planner.update_pioneer_waypoint(ctd_data)
-            t2 = time()
-            print("Update pioneer waypoint takes: ", t2 - t1)
 
-            # s4: update simulation data
-            self.log.append_log(self.grf)
+            for i in range(num_steps):
+            # for i in range(len(wps)):
+                t2 = time()
+                print("Step: ", i, " takes ", t2 - t1, " seconds. ")
+                t1 = time()
 
-        # plt.plot(rmse)
-        # plt.title("RMSE in")
-        # plt.show()
+                """ plotting seciton. """
+                if self.debug:
+                    plt.figure(figsize=(15, 12))
+                    cost_valley = cv.get_cost_field()
+                    plotf_vector(grid[:, 1], grid[:, 0], cost_valley, xlabel='East', ylabel='North', title='RRTCV',
+                                 cbar_title="Cost", cmap=get_cmap("RdBu", 10), vmin=0, vmax=2.2, stepsize=.25)
+                    if len(trajectory) > 0:
+                        plt.plot(trajectory[:, 1], trajectory[:, 0], 'k.-')
+                    plt.plot(polygon_border[:, 1], polygon_border[:, 0], 'r-.')
+                    plt.plot(polygon_obstacle[:, 1], polygon_obstacle[:, 0], 'r-.')
+                    plt.xlabel("East")
+                    plt.ylabel("North")
+                    plt.savefig(figpath + "P_{:03d}.png".format(i))
+                    plt.close("all")
 
-        return trajectory, self.log
+                planner.update_planning_trackers()
+
+                # p1: parallel move AUV to the first location
+                wp_now = planner.get_current_waypoint()
+                trajectory = np.append(trajectory, wp_now.reshape(1, -1), axis=0)
+
+                # s2: obtain CTD data
+                ctd_data = self.ctd.get_ctd_data(wp_now)
+
+                # s3: update pioneer waypoint
+                t1 = time()
+                # print("CTD: ", ctd_data)
+                planner.update_pioneer_waypoint(ctd_data)
+                t2 = time()
+                print("Update pioneer waypoint takes: ", t2 - t1)
+
+                # s4: update simulation data
+                log.append_log(grf)
+
+            traj_sim = np.append(traj_sim, trajectory.reshape(1, num_steps + 1, 2), axis=0)
+            rmse = np.append(rmse, np.array(log.rmse).reshape(1, -1), axis=0)
+            ibv = np.append(ibv, np.array(log.ibv).reshape(1, -1), axis=0)
+            vr = np.append(vr, np.array(log.vr).reshape(1, -1), axis=0)
+
+        return traj_sim, rmse, ibv, vr
 
 
 if __name__ == "__main__":
     s = Simulator(debug=True)
-    s.set_weights(1.9, .1)
-    t1, l1 = s.run_simulator(num_steps=10)
-    s.set_weights(1., 1.)
-    t2, l2 = s.run_simulator(num_steps=10)
-    s.set_weights(.1, 1.9)
-    t3, l3 = s.run_simulator(num_steps=10)
+    t, r, i, v = s.run_simulator(num_steps=3)
+    t, r
 
-    t1, l1
--- Publication/src/run_cv.py
+++ Publication/src/run_cv.py
@@ -8,9 +8,10 @@ from time import time
 import numpy as np
 
 config = Config()
-num_steps = config.get_num_steps()
 num_replicates = config.get_num_replicates()
 num_cores = config.get_num_cores()
+num_steps = config.get_num_steps()
+reps = 1 * np.ones(num_replicates).astype(int)
 
 """
 Return values are tuple and hereby need careful check with smaller steps 
@@ -18,49 +19,28 @@ of replicates to extract the result correctly.
 """
 
 
-def run_replicates(weight_eibv=1., weight_ivr=1.):
+def run_replicates(i):
+    print("R: ", i)
     s = Simulator()
-    steps = num_steps * np.ones(num_replicates).astype(int)
-
-    if weight_eibv > weight_ivr:
-        case = "EIBV"
-    elif weight_eibv < weight_ivr:
-        case = "IVR"
-    else:
-        case = "EQUAL"
-
-    s.set_weights(weight_eibv, weight_ivr)
-
-    res = Parallel(n_jobs=num_cores)(delayed(s.run_simulator)(step) for step in steps)  # The return values are tuple
-
-    traj_sim = np.empty([0, num_steps+1, 2])
-    ibv = np.empty([0, num_steps+1])
-    rmse = np.empty([0, num_steps+1])
-    vr = np.empty([0, num_steps+1])
-
-    for i in range(len(res)):
-        traj_sim = np.append(traj_sim, res[i][0].reshape(1, num_steps+1, 2), axis=0)
-        rmse = np.append(rmse, np.array(res[i][1].rmse).reshape(1, -1), axis=0)
-        ibv = np.append(ibv, np.array(res[i][1].ibv).reshape(1, -1), axis=0)
-        vr = np.append(vr, np.array(res[i][1].vr).reshape(1, -1), axis=0)
-
-    # t1 = time()
-    # for i in range(num_replicates):
-    #     t2 = time()
-    #     print("Replicate: ", i, " takes ", t2 - t1, " seconds.")
-    #     t1 = time()
-    #     traj = run_simulator()
-    #     traj_sim = np.append(traj_sim, traj.reshape(1, num_steps+1, 2), axis=0)
-
-    np.save("npy/CV/" + case + ".npy", traj_sim)
-    np.save("npy/CV/" + case + "_ibv.npy", ibv)
-    np.save("npy/CV/" + case + "_vr.npy", vr)
-    np.save("npy/CV/" + case + "_rmse.npy", rmse)
-    return 0
+    traj, rmse, ibv, vr = s.run_simulator(num_steps)
+    return traj, rmse, ibv, vr
 
 
 if __name__ == "__main__":
-    run_replicates(1.9, .1)
-    run_replicates(1., 1.)
-    run_replicates(.1, 1.9)
+    traj_sim = np.empty([0, 3, num_steps + 1, 2])
+    ibv = np.empty([0, 3, num_steps + 1])
+    rmse = np.empty([0, 3, num_steps + 1])
+    vr = np.empty([0, 3, num_steps + 1])
 
+    res = Parallel(n_jobs=num_cores)(delayed(run_replicates)(rep) for rep in reps)
+
+    for i in range(len(res)):
+        traj_sim = np.append(traj_sim, res[i][0].reshape(1, 3, num_steps + 1, 2), axis=0)
+        rmse = np.append(rmse, res[i][1].reshape(1, 3, num_steps + 1), axis=0)
+        ibv = np.append(ibv, res[i][2].reshape(1, 3, num_steps + 1), axis=0)
+        vr = np.append(vr, res[i][3].reshape(1, 3, num_steps + 1), axis=0)
+
+    np.save("npy/CV/TRAJ.npy", traj_sim)
+    np.save("npy/CV/RMSE.npy", rmse)
+    np.save("npy/CV/IBV.npy", ibv)
+    np.save("npy/CV/VR.npy", vr)
