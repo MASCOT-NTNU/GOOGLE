4be3996d0a096ce1cd59331836952315fa50c11b
Yaolin Ge <geyaolin@gmail.com>
2023-01-06 17:04:02 +0100

    update_setup

--- Publication/src/Config.py
+++ Publication/src/Config.py
@@ -43,9 +43,9 @@ class Config:
         self.__loc_start = np.array([x, y])
 
         """ Default simulation parameter seteup. """
-        self.__num_steps = 5  # number of steps.
-        self.__num_replicates = 5  # number of replicates
-        self.__num_cores = 5  # number of cores to use
+        self.__num_steps = 100  # number of steps.
+        self.__num_replicates = 100  # number of replicates
+        self.__num_cores = 45  # number of cores to use
 
     @staticmethod
     def wgs2xy(value: np.ndarray) -> np.ndarray:
