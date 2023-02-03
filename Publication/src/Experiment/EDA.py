ed1d0856037e5ab8bb0f63b6d7eacdb970ef9ba2
Yaolin Ge <geyaolin@gmail.com>
2023-02-03 10:05:11 +0100

    update_paper_figure_ploting

--- Publication/src/Experiment/EDA.py
+++ Publication/src/Experiment/EDA.py
@@ -232,6 +232,7 @@ class EDA:
         lons = np.array(xs).flatten()
         lats = np.array(ys).flatten()
 
+        # TODO: check if it is needed or remove it?
         lats
         pass
 
@@ -240,13 +241,18 @@ class EDA:
         It uses the dataset synchronized from AUV to assimilate the kernel for the GRF model.
         - It generates plots for the mission review.
         - It saves data to csv files later can be used for GIS visualisation.
-
+        - It updates the cost valley based on the posterior field.
         """
+        sigma = 1.5
+        nugget = .4
+        rrtstar = RRTStarCV(weight_eibv=.5, weight_ivr=.5, sigma=sigma, nugget=nugget, budget_mode=True)
+        cv = rrtstar.get_CostValley()
+        grf = cv.get_grf_model()
+
         step_auv = 170  # samples between two waypoints.
         df = self.auv.get_dataset()
         n_samples = len(df)
-        threshold = self.grf.get_threshold()
-        sigma = self.grf.get_sigma()
+        threshold = grf.get_threshold()
 
         def plot_each_component(value, traj: np.ndarray, ind_assimilated: np.ndarray, ind_gathered: np.ndarray,
                                 title: str = "Salinity", cmap=get_cmap("BrBG", 10), cbar_title="Salinity",
@@ -273,26 +279,32 @@ class EDA:
             """
             start plotting section
             """
-            mu = self.grf.get_mu()
-            std = np.sqrt(np.diag(self.grf.get_covariance_matrix()))
-            fig = plt.figure(figsize=(40, 12))
-            gs = GridSpec(nrows=1, ncols=3)
+            mu = grf.get_mu()
+            # std = np.sqrt(np.diag(grf.get_covariance_matrix()))
+            fig = plt.figure(figsize=(30, 15))
+            gs = GridSpec(nrows=1, ncols=2)
             ax = fig.add_subplot(gs[0])
             xp, yp, v_mu = plot_each_component(mu, traj=traj, ind_assimilated=ind_assimilated,
-                                               ind_gathered=ind_gathered, title="Conditional mean", threshold=threshold)
-            ax = fig.add_subplot(gs[1])
-            xp, yp, v_std = plot_each_component(std, traj=traj, ind_assimilated=ind_assimilated,
-                                                ind_gathered=ind_gathered,
-                                title="Conditional std", cbar_title="STD", cmap=get_cmap("RdBu", 10),
-                                vmin=0, vmax=sigma + .1, stepsize=.1)
-            ax = fig.add_subplot(gs[2])
-            ep = norm.cdf(threshold, mu.flatten(), std)
-            xp, yp, v_ep = plot_each_component(ep, traj=traj, ind_assimilated=ind_assimilated,
-                                               ind_gathered=ind_gathered,
-                                title="Conditional EP", cbar_title="Probability", cmap=get_cmap("YlGnBu", 10),
-                                vmin=0, vmax=1.01, stepsize=.1, threshold=.5)
+                                               ind_gathered=ind_gathered, title="Updated salinity field", threshold=threshold)
 
-            figpath = self.figpath + "ReCap/"
+            ax = fig.add_subplot(gs[1])
+            cost_field = cv.get_cost_field()
+            xp, yp, v_cv = plot_each_component(cost_field, traj=traj, ind_assimilated=ind_assimilated,
+                                               ind_gathered=ind_gathered, title="Updated cost valley",
+                                               cbar_title="Cost", cmap=get_cmap("RdBu", 10), vmin=0, vmax=2.,
+                                               stepsize=.1)
+            # xp, yp, v_std = plot_each_component(std, traj=traj, ind_assimilated=ind_assimilated,
+            #                                     ind_gathered=ind_gathered,
+            #                     title="Conditional std", cbar_title="STD", cmap=get_cmap("RdBu", 10),
+            #                     vmin=0, vmax=sigma + .1, stepsize=.1)
+            # ax = fig.add_subplot(gs[2])
+            # ep = norm.cdf(threshold, mu.flatten(), std)
+            # xp, yp, v_ep = plot_each_component(ep, traj=traj, ind_assimilated=ind_assimilated,
+            #                                    ind_gathered=ind_gathered,
+            #                     title="Conditional EP", cbar_title="Probability", cmap=get_cmap("YlGnBu", 10),
+            #                     vmin=0, vmax=1.01, stepsize=.1, threshold=.5)
+
+            figpath = self.figpath + "ReCapCV/"
             checkfolder(figpath)
             plt.savefig(figpath + "/P_{:03d}.png".format(counter))
             plt.close("all")
@@ -309,9 +321,12 @@ class EDA:
                 ind_start = i
                 ind_end = -1
 
-            ind_assimilated, val_assimilated = self.grf.assimilate_temporal_data(df[ind_start:ind_end])
+            ind_assimilated, val_assimilated = grf.assimilate_temporal_data(df[ind_start:ind_end])
             traj = df[:ind_end, 1:-1]
             ind_gathered = np.append(ind_gathered, ind_assimilated.reshape(-1, 1), axis=0)
+
+            loc_now = df[ind_end, 1:-1]
+            cv.update_cost_valley(loc_now=loc_now)
 
             counter += 1
 
