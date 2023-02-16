dd3d45dfd352a63953361b76841f2a4a0fe1cb2f
Yaolin Ge <geyaolin@gmail.com>
2023-02-16 15:42:16 +0100

    learning_EIBV

--- Publication/src/tests/test_eibv_methods.py
+++ Publication/src/tests/test_eibv_methods.py
@@ -3,7 +3,8 @@ This test checks if two methods produce the same result and time analysis
 """
 from unittest import TestCase
 from GRF.GRF import GRF
-from usr_func.EIBV import EIBV_mvn, EIBV_norm
+# from usr_func.EIBV import EIBV_mvn, EIBV_norm
+from scipy.stats import multivariate_normal, norm
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
@@ -11,40 +12,118 @@ from tqdm import tqdm
 
 
 class TestEIBV(TestCase):
+
     def setUp(self) -> None:
         """ Set the parameters. """
         self.sigma = 1
         self.nugget = .4
-        self.grf = GRF(self.sigma, self.nugget)
-        self.mu = self.grf.get_mu()
-        self.cov = self.grf.get_covariance_matrix()
-        self.threshold = self.grf.get_threshold()
+        # self.grf = GRF(self.sigma, self.nugget)
+        # self.mu = self.grf.get_mu()
+        # self.cov = self.grf.get_covariance_matrix()
+        # self.threshold = self.grf.get_threshold()
 
-    def test_eibv_calculations(self) -> None:
-        """
-        Compare the results from two different implementations.
-        """
-        eibv_mvn = np.zeros_like(self.mu)
-        eibv_norm = np.zeros_like(self.mu)
 
-        t_mvn = []
-        t_norm = []
-        for i in tqdm(range(len(self.mu))):
-            H = np.zeros_like(self.mu).T
-            H[0, i] = True
+    def test_eibv(self) -> None:
 
-            t1 = time()
-            eibv_mvn[i] = EIBV_mvn(self.threshold, self.mu, self.cov, H, self.nugget)
-            t2 = time()
-            t_mvn.append(t2 - t1)
+        """ Section I: check correlation plot. """
+        # mu = np.zeros(2)
+        # Cov = np.array([[1, .84],
+        #                 [.84, 1]])
+        # CovN = np.array([[1, -.84],
+        #                  [-.84, 1]])
+        # x1 = np.arange(-3, 3, .1)
+        # x2 = np.arange(-3, 3, .1)
+        #
+        # xx1, xx2 = np.meshgrid(x1, x2)
+        #
+        # Fx = np.zeros_like(xx1)
+        # px = np.zeros_like(Fx)
+        # qx = np.zeros_like(px)
+        #
+        # for i in range(xx1.shape[0]):
+        #     for j in range(xx2.shape[1]):
+        #         Fx[i, j] = multivariate_normal.cdf(np.array([xx1[i, j], xx2[i, j]]), mu, Cov)
+        #         px[i, j] = multivariate_normal.pdf(np.array([xx1[i, j], xx2[i, j]]), mu, Cov)
+        #         qx[i, j] = multivariate_normal.pdf(np.array([xx1[i, j], xx2[i, j]]), mu, CovN)
+        #
+        # " Here comes the plotting section. "
+        # def plotf_contour(x, y, v, title):
+        #     plt.figure()
+        #     c = plt.contour(x, y, v)
+        #     plt.gca().clabel(c, inline=True, fontsize=10)
+        #     plt.title(title)
+        #     plt.xlabel(r"$Z_a$")
+        #     plt.ylabel(r"$Z_b$")
+        #     plt.gca().set_aspect("equal")
+        #     plt.show()
 
-            t1 = time()
-            eibv_norm[i] = EIBV_norm(self.threshold, self.mu, self.cov, H, self.nugget)
-            t2 = time()
-            t_norm.append(t2 - t1)
+        # plotf_contour(xx1, xx2, Fx, "Cumulative density function")
+        # plotf_contour(xx1, xx2, px, "Probability density function, positive correlation")
+        # plotf_contour(xx1, xx2, qx, "Probability density function, negative correlation")
 
-        print("MVN takes: ", np.sum(t_mvn) / len(t_mvn))
-        print("Norm takes: ", np.sum(t_norm) / len(t_norm))
-        print("Result absolute discrepancy: ", np.sum(np.abs(eibv_mvn - eibv_norm)))
+
+        """ Section II: check EIBV calculation. """
+        m = np.array([[8]])  # mean
+        T = np.array([[8.1]])  # threshold
+        s = np.array([[.8]])  # latent std
+        tt = np.array([[.5]])  # nugget std
+        cco = np.array([[.9]])  # correlation
+        sig2 = s**2 + tt**2
+        sig = np.sqrt(sig2)
+
+        ''' Prior ibv '''
+        ibv = norm.cdf((T - m)/s) * norm.cdf((m - T)/s)
+
+        k = cco * s ** 2
+        sn2 = s**2 - k @ np.linalg.solve(sig2, k)
+        sn = np.sqrt(sn2)
+
+        a = (m - T) / sn
+        b = 1 / sn @ np.linalg.solve(sig2, k.T)
+        c = 1 + b.T * sig2 * b
+
+        ''' MC '''
+        B = 1000000
+        u = sig * np.random.randn(B)
+        IntMC = np.mean(norm.cdf(a + b * u) * norm.cdf(-a - b * u))
+
+
+        ''' Analytical - version 1 (Chevalier et al., 2014) '''
+        IntA = multivariate_normal.cdf(np.array([a, a]).squeeze(), [0, 0], np.array([[c, 1-c],
+                                                                                     [1-c, c]]).squeeze())
+
+
+        px
+
+        pass
+
+
+
+    # def test_eibv_calculations(self) -> None:
+    #     """
+    #     Compare the results from two different implementations.
+    #     """
+    #     eibv_mvn = np.zeros_like(self.mu)
+    #     eibv_norm = np.zeros_like(self.mu)
+    #
+    #     t_mvn = []
+    #     t_norm = []
+    #     for i in tqdm(range(len(self.mu))):
+    #         H = np.zeros_like(self.mu).T
+    #         H[0, i] = True
+    #
+    #         t1 = time()
+    #         eibv_mvn[i] = EIBV_mvn(self.threshold, self.mu, self.cov, H, self.nugget)
+    #         t2 = time()
+    #         t_mvn.append(t2 - t1)
+    #
+    #         t1 = time()
+    #         eibv_norm[i] = EIBV_norm(self.threshold, self.mu, self.cov, H, self.nugget)
+    #         t2 = time()
+    #         t_norm.append(t2 - t1)
+    #
+    #     print("MVN takes: ", np.sum(t_mvn) / len(t_mvn))
+    #     print("Norm takes: ", np.sum(t_norm) / len(t_norm))
+    #     print("Result absolute discrepancy: ", np.sum(np.abs(eibv_mvn - eibv_norm)))
 
 
